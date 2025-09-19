afd01cd8fb21ded58b4c34bd80d07ee5067779e4
yoos23 <benjamin.taymans2811@gmail.com>
2025-09-19 18:55:52 +0200

    Update BakuHelmetModel.java

--- src/main/java/com/mythicfactions/BakuHelmetModel.java
+++ src/main/java/com/mythicfactions/BakuHelmetModel.java
@@ -4,16 +4,16 @@ import software.bernie.geckolib.model.GeoModel;
 public class BakuHelmetModel extends GeoModel<BakuHelmetItem> {
     @Override
     public Identifier getModelResource(BakuHelmetItem object) {
-        return new Identifier("mythicfactions:baku_helmet.geo.json");
+        return new Identifier("mythicfactions", "geo/baku_helmet.geo.json");
     }
 
     @Override
     public Identifier getTextureResource(BakuHelmetItem object) {
-        return new Identifier("mythicfactions:textures/armor/baku_helmet.png");
+        return new Identifier("mythicfactions", "textures/armor/baku_helmet.png");
     }
 
     @Override
     public Identifier getAnimationResource(BakuHelmetItem animatable) {
-        return new Identifier("mythicfactions:animations/baku_helmet.animation.json");
+        return new Identifier("mythicfactions", "animations/baku_helmet.animation.json");
     }
 }
