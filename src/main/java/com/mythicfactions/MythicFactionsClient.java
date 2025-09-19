b0dd0521073e6b5c7f8dc244c6903ab06e95973f
yoos23 <benjamin.taymans2811@gmail.com>
2025-09-19 18:51:57 +0200

    Update MythicFactionsClient.java

--- src/main/java/com/mythicfactions/MythicFactionsClient.java
+++ src/main/java/com/mythicfactions/MythicFactionsClient.java
@@ -1,11 +1,8 @@
-package com.mythicfactions;
-
-import net.fabricmc.api.ClientModInitializer;
 import software.bernie.geckolib.renderer.GeoArmorRenderer;
 
 public class MythicFactionsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        GeoArmorRenderer.registerRenderer(BakuHelmetItem.class, BakuHelmetRenderer::new);
+        GeoArmorRenderer.register(BakuHelmetItem.class, () -> new BakuHelmetRenderer());
     }
 }
