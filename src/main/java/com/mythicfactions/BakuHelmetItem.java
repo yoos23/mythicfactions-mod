10611f5f15ac158b6e3e77196b8daa37424cd1b1
yoos23 <benjamin.taymans2811@gmail.com>
2025-09-19 19:00:28 +0200

    Update BakuHelmetItem.java

--- src/main/java/com/mythicfactions/BakuHelmetItem.java
+++ src/main/java/com/mythicfactions/BakuHelmetItem.java
@@ -3,14 +3,18 @@ import net.minecraft.item.ArmorMaterials;
 import net.minecraft.item.Item;
 import software.bernie.geckolib.animatable.GeoAnimatable;
 import software.bernie.geckolib.item.GeoArmorItem;
+import software.bernie.geckolib.util.GeckoLibUtil;
+import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
 
 public class BakuHelmetItem extends GeoArmorItem implements GeoAnimatable {
+    private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
+
     public BakuHelmetItem() {
         super(ArmorMaterials.NETHERITE, ArmorItem.Type.HELMET, new Item.Settings());
     }
 
     @Override
-    public double getTick(Object object) {
-        return 0;
+    public AnimatableInstanceCache getAnimatableInstanceCache() {
+        return cache;
     }
 }
